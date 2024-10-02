1ff27613b0d084ec1e0fbc61103b1fb748b73917
jandel <alunoyandelbermudezalonso@gmail.com>
2024-10-02 08:05:11 -0300

    first commit

---
+++ python.py
@@ -0,0 +1,40 @@
+print ('seja bem vindo ao banco Bradesco')
+
+nome = input('para começar digite seu nome')
+
+print ('seu nome é', nome)
+
+cpf = input('digite seu cpf')
+
+print ('seu cpf é', cpf)
+
+print (nome, cpf)
+
+print ('Bem vindo', nome)
+
+pregunta = input('se deseja consultar seu saldo digite 1')
+
+if pregunta == '1':
+    input('seu saldo é de 4000 reais')
+
+despedida = input('si deseja despedise digite 2')
+
+if despedida == '2':
+    input('obrigado por utilizar nossos serviços yandel')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
